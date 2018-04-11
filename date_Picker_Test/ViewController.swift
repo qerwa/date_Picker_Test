70146b991a0719d40ce566704fdc891604701648
D7703_17 <d7703_17@d7703-17ui-imac.local>
2018-04-11 15:33:17 +0900

    date picker

--- date_Picker_Test/ViewController.swift
+++ date_Picker_Test/ViewController.swift
@@ -9,17 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+      @IBOutlet weak var picker: UIDatePicker!
+      @IBOutlet weak var label: UILabel!
+      
       override func viewDidLoad() {
             super.viewDidLoad()
             // Do any additional setup after loading the view, typically from a nib.
       }
 
-      override func didReceiveMemoryWarning() {
-            super.didReceiveMemoryWarning()
-            // Dispose of any resources that can be recreated.
+      @IBAction func picker(_ sender: Any) {
+            print("change Date Picker")
+            
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy-MM-dd HH:mm:ss a EE"
+            label.text = formatter.string(from: picker.date)
+            
       }
-
-
+      
 }
 
