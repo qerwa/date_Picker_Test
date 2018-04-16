6293c45e7e9632cf43f90f6826426094ae17f405
D7703_17 <d7703_17@d7703-17ui-imac.local>
2018-04-16 14:46:53 +0900

    add bt

--- date_Picker_Test/ViewController.swift
+++ date_Picker_Test/ViewController.swift
@@ -19,20 +19,28 @@ class ViewController: UIViewController {
             // Do any additional setup after loading the view, typically from a nib.
             myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
       }
-
+      @IBAction func bt(_ sender: Any) {
+            view.backgroundColor = UIColor.white
+      }
+      
       @IBAction func picker(_ sender: Any) {
             print("change Date Picker")
             
             let formatter = DateFormatter()
-            formatter.dateFormat = "yyyy-MM-dd HH:mm:ss a EE"
+            formatter.dateFormat = "yyyy/MM/dd HH:mm:ss a EEEE"
             label.text = formatter.string(from: picker.date)
             
       }
       @objc func updateTime() {
             let date = Date()
             let formatter = DateFormatter()
-            formatter.dateFormat = "HH:mm:ss EEE"
+            formatter.dateFormat = "YYYY/MM/dd HH:mm:ss a EEEE"
             NT.text = formatter.string(from: date)
+            
+            if(NT.text == label.text){
+                  view.backgroundColor = UIColor.yellow
+            }
+            
       }
       
 }
